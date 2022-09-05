3fd4e4bd5588bd3b5cd16c918c8665ed3a2bb7d1
recordame <recordame@msn.com>
2022-09-05 16:35:52 +0900

    내일 날씨정보 추가
    내일 날씨 추가

--- weather.py
+++ weather.py
@@ -10,10 +10,8 @@ from mmpy_bot import Plugin, listen_to
 import constant
 
 
-## 날씨 정보 얻기
-def weather_info(loc: str = ""):
-    now = datetime.datetime.now()
-
+## 전체 날씨 정보
+def get_info(loc: str = ""):
     # 네이버 날씨
     url: str = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="
     if loc == "":
@@ -26,12 +24,20 @@ def weather_info(loc: str = ""):
     html = page.read().decode('utf-8')
     soup = bs4.BeautifulSoup(html, "html.parser")
 
-    location = soup.find('div', class_="title_area").find('h2').text
-    today = soup.find('div', class_="weather_info").find('div', class_="status_wrap").find('div', class_="_today")
+    return soup
+
+
+## 오늘 날씨 정보
+def get_today(info: str):
+    now = datetime.datetime.now()
+
+    location = info.find('div', class_="title_area").find('h2').text
+    today = info.find('div', class_="weather_info").find('div', class_="status_wrap").find('div', class_="_today")
     status = today.find('div', class_="weather_main").find('span', class_="blind").text
     temp = today.find('div', class_="temperature_text").text.replace("현재 온도", "").replace(" ", "")
     temp_yesterday = today.find('div', class_="temperature_info").find('span', class_="temperature").text.removesuffix(" ")
-    summary_list = today.find('dl', class_="summary_list").text.strip(" ").split(" ")
+    feel_humidity_wind = today.find('dl', class_="summary_list").text.strip(" ").split(" ")
+    particle_uv_sunset = info.find('div', class_="report_card_wrap").text.strip(" ").split(" ")
 
     # 현재 날씨
     month = str(int(now.strftime("%m")))
@@ -40,21 +46,72 @@ def weather_info(loc: str = ""):
     minute = str(int(now.strftime("%M")))
     second = str(int(now.strftime("%S")))
 
-    msg = "@here **" + now.strftime(month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + second + "초") + "**" \
-          + " 현재 **" + location + "**의 날씨 입니다.\n" \
+    msg = "``" + now.strftime(month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + second + "초") + "``\n" \
+          + "현재 ``" + location + "``의 날씨 입니다.\n" \
           + "- 현재 : " + status + "\n" \
           + "- 기온 : " + temp + "(어제보다 " + temp_yesterday + ")" + "\n"
 
     # 체감, 습도, 바람 상태
     tmp: str = ""
     i: int = 0
-    while i < len(summary_list):
+
+    while i < len(feel_humidity_wind):
         if i % 2 == 0:
-            tmp += "- " + summary_list[i] + " : "
-        elif i == len(summary_list) - 1:
-            tmp += summary_list[i]
+            tmp += "- " + feel_humidity_wind[i] + " : "
         else:
-            tmp += summary_list[i] + "\n"
+            tmp += feel_humidity_wind[i] + "\n"
+        i += 1
+
+    # 미세먼지, 초미세먼지, 자외선, 일몰
+    i = 0
+    while i < len(particle_uv_sunset):
+        if particle_uv_sunset[i] != "":
+            if i % 2 == 0:
+                tmp += "- " + particle_uv_sunset[i] + " : "
+            elif i == len(particle_uv_sunset) - 1:
+                tmp += particle_uv_sunset[i]
+            else:
+                tmp += particle_uv_sunset[i] + "\n"
+
+        i += 1
+
+    msg += tmp
+
+    return msg
+
+
+## 내일 날씨 정보
+def get_tomorrow(info: str):
+    status_am = info.find('div', class_="weather_info type_tomorrow").find('ul', class_="weather_info_list _tomorrow").find('div', class_="weather_main").text.strip(" ")
+    temp_am = info.find('div', class_="weather_info type_tomorrow").find('ul', class_="weather_info_list _tomorrow").find('div', class_="temperature_text").text.replace("예측 온도", "").replace(" ", "")
+    precipitation_am = info.find('div', class_="weather_info type_tomorrow").find('ul', class_="weather_info_list _tomorrow").find('dl', class_="summary_list").text.replace("강수확률", "").strip(" ")
+    particle = info.find('div', class_="weather_info type_tomorrow").find('ul', class_="weather_info_list _tomorrow").find('div', class_="report_card_wrap").text.strip(" ").split(" ")
+    status_pm = info.find('div', class_="weather_info type_tomorrow").find('div', class_="_pm").find('div', class_="weather_main").text.strip(" ")
+    temp_pm = info.find('div', class_="weather_info type_tomorrow").find('div', class_="_pm").find('div', class_="temperature_text").text.replace("예측 온도", "").replace(" ", "")
+    precipitation_pm = info.find('div', class_="weather_info type_tomorrow").find('div', class_="_pm").find('dd', class_="desc").text
+
+    msg = "내일 예상 날씨 입니다.\n" \
+          + "- 오전\n" \
+          + "   - 예상 : " + status_am + "\n" \
+          + "   - 기온 : " + temp_am + "\n" \
+          + "   - 강수 : " + precipitation_am + "\n" \
+          + "- 오후\n" \
+          + "   - 예상 : " + status_pm + "\n" \
+          + "   - 기온 : " + temp_pm + "\n" \
+          + "   - 강수 : " + precipitation_pm + "\n"
+
+    # 미세먼지, 초미세먼지
+    tmp: str = ""
+    i = 0
+    while i < len(particle):
+        if particle[i] != "":
+            if i % 2 == 0:
+                tmp += "- " + particle[i] + " : "
+            elif i == len(particle) - 1:
+                tmp += particle[i]
+            else:
+                tmp += particle[i] + "\n"
+
         i += 1
 
     msg += tmp
@@ -63,9 +120,23 @@ def weather_info(loc: str = ""):
 
 
 class Weather(Plugin):
-    ## 스켸쥴링시 날씨 정보를 갱신해서 가지고 오는 부분
+    @listen_to("^날씨$")
+    def weather(self, message: Message, loc: str = ""):
+        info = get_info(loc);
+        msg = get_today(info) + "\n" + get_tomorrow(info)
+
+        self.driver.direct_message(message.user_id, msg)
+
+    ## 필요시
+    @listen_to("^날씨 ([가-힣]+)$")
+    def weather_at_location(self, message: Message, loc: str):
+        self.weather(message, loc)
+
+    ## 스케줄링시 날씨 정보를 갱신해서 가지고 오는 부분
     def alarm_funcs(self, message: Message):
-        msg = weather_info()
+        info = get_info();
+        msg = get_today(info) + "\n" + get_tomorrow(info)
+
         self.driver.create_post(constant.CH_NOTIFICATIONS_ID, msg)
 
     ## 스케쥴링
@@ -81,12 +152,3 @@ class Weather(Plugin):
     def cancel_jobs(self, message: Message):
         schedule.clear()
         self.driver.direct_message(message.user_id, "날씨 알림이 종료되었습니다.")
-
-    ## 필요시
-    @listen_to("^날씨 ([가-힣]+)$")
-    def weather_loc(self, message: Message, loc: str):
-        self.weather(message, loc)
-
-    @listen_to("^날씨$")
-    def weather(self, message: Message, loc: str = ""):
-        self.driver.direct_message(message.user_id, weather_info(loc))
