e9eac3ac83d424f00cb27983a645ea86577e1de5
recordame <recordame@msn.com>
2022-09-06 17:24:32 +0900

    코드정리

--- process.py
+++ process.py
@@ -1,4 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from mmpy_bot import Message
 from mmpy_bot import Plugin, listen_to
 
@@ -6,13 +5,13 @@ import constant
 
 
 class Process(Plugin):
-    schedule = BackgroundScheduler()
 
-    @listen_to("^프로세스정보$")
+    # 프로세스 정보 출력
+    @listen_to("^프로세스$")
     def notify(self, message: Message):
         msg: str = ""
 
         for i in constant.JOBS:
             msg += "``" + str(i) + "``\n"
 
-        self.driver.direct_message(message.user_id, "현재 백그라운드에서 작동중인 스케쥴러 : " + str(constant.JOBS.__len__()) + "개\n" + msg)
+        self.driver.direct_message(message.user_id, "Background Jobs : " + str(constant.JOBS.__len__()) + "개\n" + msg)
